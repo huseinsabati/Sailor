bc55b011357e31e54cba8f49cadad0771e5a7b4e
huseinsabati <62718703+huseinsabati@users.noreply.github.com>
2024-11-14 12:18:17 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1000, // Set the limit in KB (default is 500 KB)
+  },
   plugins: [react()],
   server: {
     proxy: {
